2aaca0cca2fd1c4c0b09aa37ecba560b8effbb62
Kurtis Toledo <kurtistoledo@gmail.com>
2024-03-22 21:16:13 -0500

    Task2_2.py

---
+++ Task2_2.py
@@ -0,0 +1,12 @@
+# 2. Quick Decisions: The Event Planner
+# Task 2: Venue Selection
+attendees_count = input("Enter number of attendees: ")
+attendees = int(attendees_count)
+venue = "large hall" if attendees > 50 else "conference room"
+additional_facilities = ["audio system", "projector"] if attendees > 100 else ["audio system"] if attendees > 50 else []
+
+print("Venue:", venue)
+if additional_facilities:
+    print("Additional Facilities Needed:", ", ".join(additional_facilities))
+else:
+    print("No additional facilities needed.")
